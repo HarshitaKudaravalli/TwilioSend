94498b01f1ac73539c1f01d1e3851a485493529e
Harshita Kudaravalli <reach.harshitha@gmail.com>
2017-10-04 20:30:04 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 // Use the REST API Client to make requests to the Twilio REST API
 use Twilio\Rest\Client;
-
+echo "<h1> A message is sent </h1>";
 // Your Account SID and Auth Token from twilio.com/console
 $sid = 'AC5041ae87c764d597ecb632db5a55391c';
 $token = 'c5424fb9a0b4fe03136edf197593246d';
@@ -19,7 +19,8 @@ $client->messages->create(
         // A Twilio phone number you purchased at twilio.com/console
         'from' => '+14242303252',
         // the body of the text message you'd like to send
-        'body' => "hi. I'm harshitha"
+        'body' => "Hello from Harshita Kudaravalli CS643 Fall 2017"
     )
 );
+echo "<p> To know more about this service <a href='1.html'>Click Here</a></p>";
 ?>
